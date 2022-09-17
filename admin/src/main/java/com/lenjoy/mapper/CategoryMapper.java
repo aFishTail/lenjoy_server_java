bf1ab354640bbfaddc3785f2156bda0fa5c8f694
hebai <949491794@qq.com>
2022-09-17 15:14:50 +0800

    delete before

--- admin/src/main/java/com/lenjoy/mapper/CategoryMapper.java
+++
@@ -1,7 +0,0 @@
-package com.lenjoy.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lenjoy.entity.Category;
-
-public interface CategoryMapper extends BaseMapper<Category> {
-}
